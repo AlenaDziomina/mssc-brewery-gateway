496f30e961d92a18e29485ddd853beea26d77342
Alena Dziomina <alenadziomina@gmail.com>
2021-04-18 23:09:08 +0200

    Initial pom

---
+++ src/main/java/guru/springframework/msscbrewerygateway/MsscBreweryGatewayApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryGatewayApplication.class, args);
+    }
+}
